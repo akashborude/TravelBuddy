e739b1ce2f9bb978b6cb25245bd99988ac6fc28e
akashborude <akashborude@21gmail.com>
2024-01-02 20:27:02 +0530

    routing for navbar done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { DestinationsComponent } from './pages/destinations/destinations.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
-  {path: '', component:HomeComponent}
+  {path: '', component:HomeComponent},
+  {path: 'aboutus', component: AboutusComponent},
+  {path: 'destinations', component: DestinationsComponent},
+  {path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
