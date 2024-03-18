c0fa293ac4510fab38f5ae18ec00ea3337c9f21a
akashborude <akashborude@21gmail.com>
2024-03-18 17:23:26 +0530

    contact form html done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './sharepage/navbar/navbar.component';
@@ -25,6 +25,7 @@ import { ContactComponent } from './pages/contact/contact.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { SafeResourceUrl } from '@angular/platform-browser';
-import { DomSanitizer } from '@angular/platform-browser';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+ selector: 'app-contact',
+ templateUrl: './contact.component.html',
+ styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+ FormData!: FormGroup;
 
-  constructor(private sanitizer: DomSanitizer) { }
+ constructor(private builder: FormBuilder) { }
 
-  generateWhatsAppLink(groupInviteCode: string): SafeResourceUrl {
-    const url = `https://chat.whatsapp.com/${groupInviteCode}`;
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  }
+ ngOnInit() {
+    this.FormData = this.builder.group({
+      Fullname: new FormControl('', [Validators.required]),
+      Email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      Comment: new FormControl('', [Validators.required]),
+      Mobile: ['', [Validators.required, Validators.pattern("/^(\+\d{1,3}[- ]?)?\d{10}$/")]]
+    });
+ }
 
-  groupInviteCode = "Fsryp4KvaMzJ7RDPxOm1Be";
-
-  whatsappLink = this.generateWhatsAppLink(this.groupInviteCode);
-
-  ngOnInit(): void {
-  }
-
+ onSubmit(FormData: any) {
+    console.log(FormData);
+    // Here you can handle form submission, e.g., send data to a server
+ }
 }
